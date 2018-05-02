c26c32c39e88973b5d238fca716b74d1278b4ea9
Rafael Nascimento <support@marketcash.io>
2018-05-02 01:47:24 -0300

    new

---
+++ website/config.js
@@ -0,0 +1,27 @@
+var api = "http://poolhost.com:31127";
+
+var api_blockexplorer = "http://daemonhost.com:32267";
+
+var poolHost = "poolhost.com";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "/{symbol}/block/{id}";
+
+var transactionExplorer = "/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "mkt": [
+        ["mkt.selvahost.us", "http://mkt.selvahost.us:8117"],
+        ["mkt.4miner.me", "http://mkt.4miner.me:8118"],
+		["mkt.ciapool.com", "https://mkt.ciapool.com:8117"]
+	],
+};
